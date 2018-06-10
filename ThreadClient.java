ece9ca568b89c907a6903ff6855f4d3e9c7191e3
xuchen <1024436423@qq.com>
2018-06-10 15:47:52 +0800

    version_4

--- ThreadClient.java
+++ ThreadClient.java
@@ -4,33 +4,38 @@ import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
-
+/**
+ * 向服务器发送消息
+ */
 class OutToServer implements Runnable {
     private Socket client;
 
+    // 通过构造注入拿到服务器socket
     public OutToServer(Socket client) {
         this.client = client;
     }
 
+    // 向服务器发送消息
     @Override
     public void run() {
         try {
+            // 使用PrintStram流向服务器输出
             PrintStream ps = new PrintStream(client.getOutputStream());
+            // Scanner从键盘接收要发送的消息
             Scanner scanner = new Scanner(System.in);
-            scanner.useDelimiter("\n");
+            // scanner.useDelimiter("\n");
             while (true) {
-                System.out.println("请输入...");
+                // System.out.print("请输入消息：");
                 if(scanner.hasNext()) {
                     String str = scanner.next();
                     ps.println(str);
-                    // 结束
+                    // 约定输入 bye 代表本次聊天结束
                     if(str.equals("bye")) {
-                        System.out.println("客户端退出");
+                        System.out.println("退出聊天室");
                         ps.close();
                         scanner.close();
                         client.close();
                         break;
-
                     }
                 }
             }
@@ -40,10 +45,13 @@ class OutToServer implements Runnable {
     }
 }
 
-
+/**
+ * 读取服务器发送的消息
+ */
 class ReadFromServer implements Runnable {
     private Socket client;
 
+    // 通过构造注入拿到服务器socket
     public ReadFromServer(Socket client) {
         this.client = client;
     }
@@ -51,10 +59,15 @@ class ReadFromServer implements Runnable {
     @Override
     public void run() {
         try {
+            // 使用Scanner进行消息的显示
             Scanner scanner = new Scanner(client.getInputStream());
+            scanner.useDelimiter("\n");
             while (scanner.hasNext()) {
-                System.out.println("服务器发来的信息："+scanner.next());
+                System.out.println(scanner.next());
             }
+
+            // 这里不用关闭client，在发送线程中进行当前client的关闭
+            scanner.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -62,21 +75,27 @@ class ReadFromServer implements Runnable {
 }
 
 
-
 public class Client {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args){
         // 建立与 指定IP地址端口号的服务器的连接
         // client("192.168.43.250",8080);  // hj
-        client("192.168.72.1",8080);
+        client("192.168.72.1",12345);
     }
 
 
     // 先读再写
-    public static void client(String serverIP,int port) throws Exception{
-        Socket client = new Socket(serverIP, port);
-        Thread readThread = new Thread(new ReadFromServer(client));
-        Thread writeThread = new Thread(new OutToServer(client));
-        readThread.start();
-        writeThread.start();
+    public static void client(String serverIP,int port){
+        try {
+            Socket client = new Socket(serverIP, port);
+            // 读线程
+            Thread readThread = new Thread(new ReadFromServer(client));
+
+            // 写线程
+            Thread writeThread = new Thread(new OutToServer(client));
+            readThread.start();
+            writeThread.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- ThreadServer.java
+++ ThreadServer.java
@@ -1,14 +1,10 @@
 package chatroom;
 
-import javafx.css.Match;
-
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
@@ -16,64 +12,182 @@ import java.util.regex.Pattern;
 
 
 
-// 多线程
-class ExcuteClientServer implements Runnable {
-    // 接收用户连接的socket
-    private Socket client;
 
-    public ExcuteClientServer(Socket client) {
-        this.client = client;
+public class Server {
+
+    /**
+     * 使用Map存储客户端用户名与客户端socket
+     */
+    private static Map<String, Socket> clientMap = new HashMap<>();
+
+
+    /**
+     * 内部类，多线程处理客户端读写
+     */
+    static class ExcuteClientServer implements Runnable {
+        private Socket client;
+
+        /**
+         * 通过构造注入使客户端与线程连接
+         * @param client 客户端socket
+         */
+        public ExcuteClientServer(Socket client) {
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
+            try {
+                // 拿到客户端输入流
+                Scanner scanner = new Scanner(client.getInputStream());
+
+                while (true) {
+                    if(scanner.hasNext()) {
+                        String str = scanner.nextLine();
+                        // 使用正则表达式对客户端输入进行处理，将字符串首的换行剔除，方便之后的处理
+                        Pattern pattern = Pattern.compile("\r|\n|\n\r");
+                        Matcher matcher = pattern.matcher(str);
+                        str = matcher.replaceAll("");
+
+                        // 判断
+                        if(str.startsWith("userName")) {// 新用户注册，格式 userName:name
+                            String userName = str.split(":")[1];
+                            registerUser(userName, client);
+                        } else if(str.startsWith("G:")) {// 群聊，格式 G:msg
+                            String msg = str.split(":")[1];
+                            groupChat(msg);
+                        } else if(str.startsWith("P:")) {// 私聊，格式：P:user-msg
+                            String tmp = str.split(":")[1];
+                            // 私聊对象的用户名
+                            String userName = tmp.split("-")[0];
+                            // 私聊消息
+                            String msg = tmp.split("-")[1];
+                            privateChat(userName, msg);
+                        } else if(str.contains("bye")) {// 下线，格式：bye
+
+                            userExit(client);
+
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * 新用户注册
+         * @param userName 新用户用户名
+         * @param userSocket 新用户socket
+         */
+        private void registerUser(String userName, Socket userSocket) {
+            // 将新用户添加到当前用户列表中
+            clientMap.put(userName, userSocket);
+            // 向其他用户广播上线信息
+            String msg = "用户"+userName+"上线了，当前在线人数："+clientMap.size();
+            groupChat(msg);
+        }
+
+        /**
+         * 群聊
+         * @param msg 群聊发送的消息
+         */
+        private void groupChat(String msg) {
+
+            String transmit = currentUserName(client)+"说:>"+msg;
+
+            Iterator<Map.Entry<String, Socket>> iterator = clientMap.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Socket socket = iterator.next().getValue();
+                try {
+                    sendMsg(socket, transmit);
+                } catch (Exception e) {
+                    // e.printStackTrace();
+                }
+            }
+        }
+
+
+        /**
+         * 私聊
+         * @param privateUser 私聊对象用户名
+         * @param msg 私聊的消息
+         */
+        private void privateChat(String privateUser, String msg) {
+
+            Socket privateSocket = clientMap.get(privateUser);
+
+            String transmit = currentUserName(privateSocket)+"说:>"+msg;
+
+            try {
+                PrintStream printStream = new PrintStream(privateSocket.getOutputStream(), true);
+                printStream.println(transmit);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * 下线指定用户
+         * @param user 下线用户名
+         */
+        private void userExit(Socket user) {
+            String userName = currentUserName(client);
+            clientMap.remove(userName);
+            // 向所有用户通知用户下线
+            String msg = "用户"+userName+"下线，当前在线人数："+clientMap.size();
+            groupChat(msg);
+
+        }
+
+
+        /**
+         * 通过socket找到其对应的用户名
+         * @param socket 要查找的用户socket
+         * @return 对应的用户名
+         */
+        private String currentUserName(Socket socket) {
+
+            // 遍历用户列表所有用户名
+            for(String curUserName : clientMap.keySet()) {
+                if(clientMap.get(curUserName).equals(socket)) {
+                    // 拿到当前用户的用户名
+                    return curUserName;
+                }
+            }
+            return null;
+
+        }
+
+        /**
+         * 向指定socket发送信息
+         * @param socket 用户socket
+         * @param msg 发送的信息
+         */
+        private void sendMsg(Socket socket, String msg) {
+            try {
+                PrintStream printStream = new PrintStream(socket.getOutputStream(), true);
+                printStream.println(msg);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    @Override
-    public void run() {
+
+
+    public static void main(String[] args) {
         try {
-            System.out.println(client.getInetAddress()+" 已接入...");
-
-            // 向客户端写入数据
-            PrintStream ps = new PrintStream(client.getOutputStream(), true);
-            ps.print("我是服务器!!\n");
-
-
-            // 读取客户端传过来的数据
-            Scanner scanner = new Scanner(client.getInputStream());
-            scanner.useDelimiter("\n");
-            while(scanner.hasNext()) {
-                System.out.println("客户端发送："+scanner.next());
+            ServerSocket serverSocket = new ServerSocket(12345);
+            ExecutorService executorService = Executors.newFixedThreadPool(20);
+            for(int i = 0; i < 20; i++) {
+                System.out.println("等待客户端连接，本机端口："+serverSocket.getLocalPort());
+                Socket socket = serverSocket.accept();
+                executorService.execute(new ExcuteClientServer(socket));
             }
-
-            // 关闭
-            // System.out.println(client.getInetAddress()+"断开连接");
-            client.close();
+            serverSocket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
-public class Server {
-
-    public static void main(String[] args) throws Exception {
-
-        ServerSocket server = new ServerSocket(8080);
-
-        // 构建线程池
-        ExecutorService executorService = Executors.newFixedThreadPool(20);
-        for(int i = 0; i < 20; i++) {
-
-            Socket socket = null;
-            try {
-                System.out.println("等待远程连接，本机端(服务器)端口号为："+server.getLocalPort());
-                // 监听
-                socket = server.accept();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            // 将监听到的客户端加入到线程池中
-            executorService.execute(new ExcuteClientServer(socket));
-        }
-        server.close();
-    }
-}
-
